9affa431cc2c6957a94f97a94c0606557232ed17
Mateusz Grodzki <mateusz.maria.grodzki@gmail.com>
2020-01-22 11:35:46 +0100

    added PermutateTest class

---
+++ PermutateTest.java
@@ -0,0 +1,16 @@
+import or.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PermutateTest{
+
+	Permutate permutate;
+
+	@BeforEach
+	public void init(){
+		permutate = new Permutate();
+	}
+
+}
+
