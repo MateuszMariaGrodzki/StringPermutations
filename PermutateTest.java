1a72c3f3346d0b887571306fd75d583e8db9d85b
Mateusz Grodzki <mateusz.maria.grodzki@gmail.com>
2020-01-22 11:45:03 +0100

    added single dot test for generate Regex

--- PermutateTest.java
+++ PermutateTest.java
@@ -12,5 +12,17 @@ public class PermutateTest{
 		permutate = new Permutate();
 	}
 
+	@Test
+	@DisplayName("generate regex method tests for at most one dot in row");
+	void testGenerateRegexMethodSigleDot(){
+		assertAll(
+			() -> assertEquals(permutate.generateRegex("ab.ed"),"[a][b][a-z][e][d]"),
+			() -> assertEquals(permutate.generateRegex("ab.ed."),"[a][b][a-z][e][d][a-z]"),
+			() -> assertEquals(permutate.generateRegex("a.b.c"),"[a][a-z][b][a-z][c]"),
+			() -> assertEquals(permutate.generateRegex("a.b.c."),"[a][a-z][b][a-z][c][a-z]"),
+			() -> assertEquals(permutate.generateRegex(".b."),"[a-z][b][a-z]"),
+			() -> assertEquals(permutate.generateRegex(".b.d.e"),"[a-z][b][a-z][d][a-z][e]"),
+			() -> assertEquals(permutate.generateRegex(".b.d.ee."),"[a-z][b][a-z][d][a-z][e][e][a-z]")
+		);
 }
 
